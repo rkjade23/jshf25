775e3b00704b4a810e8f5a98e38e2c79904f97dc
rkjade23 <riedlkovacsj@gmail.com>
2025-03-06 19:18:53 +0100

    hf2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("valam");
+var express = require('express');
+var app = express();
+
+app.use(express.static('static'));
+
+var server = app.listen(3000, function () {
+    console.log("On: 3000");
+});
